187c992e2262127d24c2c0453f8c54352213d30f
Noufal E <noufal85@gmail.com>
2021-01-15 05:12:56 -0800

    will remove this commit

--- git_learning/git_learning.py
+++ git_learning/git_learning.py
@@ -8,4 +8,7 @@ def main():
     '''
     print("changes added from main")
 
+def added_to_be_removed():
+    print('hey , This will be removed')
+
 
---
+++ git_learning/remove_file.py
@@ -0,0 +1,6 @@
+#nothing here 
+
+def main():
+    #remove 
+    print('hello')
+
